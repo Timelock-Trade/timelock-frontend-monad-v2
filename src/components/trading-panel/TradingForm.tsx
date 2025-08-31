5edd1d381c33288df671a854e23fca0304b70a5f
Apoorv <github@libkakashi.dev>
2025-08-31 14:32:32 +0530

    disable confirm button when approval is pending

--- src/components/trading-panel/TradingForm.tsx
+++ src/components/trading-panel/TradingForm.tsx
@@ -191,7 +191,7 @@ export default function TradingForm({ isLong }: { isLong: boolean }) {
 
   const durations = filteredTtlIV.map((item) => formatDuration(item.ttl));
 
-  const { data: approvalHash, writeContractAsync: writeApproval } =
+  const { data: approvalHash, writeContractAsync: writeApproval, isPending: isApprovalPending } =
     useWriteContract();
 
   const { error: approvalError } = useWaitForTransactionReceipt({
@@ -390,7 +390,8 @@ export default function TradingForm({ isLong }: { isLong: boolean }) {
                     isSubmitting ||
                     isLoading ||
                     isError ||
-                    isPending
+                    isPending ||
+                    isApprovalPending
                   }
                 >
                   {!isConnected
